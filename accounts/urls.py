847d2a531a529330e958e22239353951085f2735
RodPy <rodneyrrspy@gmail.com>
2023-05-14 16:23:30 -0400

    ForgotPassword, methode, email

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetPassword_validate/<uidb64>/<token>/', views.resetPassword_validate, name='resetPassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
 
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
