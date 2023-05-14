ae211e30af2eb0004775bd19a6b4097bda0f73d6
RodPy <rodneyrrspy@gmail.com>
2023-05-14 18:53:02 -0400

    Logined updates the user's cart with new items.

--- accounts/views.py
+++ accounts/views.py
@@ -70,9 +70,36 @@ def login(request):
                 is_cart_item_exists =CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
                     cart_item= CartItem.objects.filter(cart=cart)
+                    
+                    product_variation=[]
+                    """
+                    This code appears to be a function that updates the user's cart with new items. 
+                    """
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                    
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_variations_list=[]
+                    id=[]
+                    for item in cart_item:
+                        existing_variation= item.variations.all()
+                        ex_variations_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_variations_list:
+                            index= ex_variations_list.index(pr)
+                            item_id= id[index]
+                            item= CartItem.objects.get(id=item_id)
+                            item.quantity +=1
+                            item.user= user
+                            item.save()
+                        else:
+                            cart_item= CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
             auth.login(request,user)
