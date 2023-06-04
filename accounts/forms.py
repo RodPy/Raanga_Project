fb0c0e0d05b0a12fc2f74f550a64c6cfec18558c
RodPy <rodneyrrspy@gmail.com>
2023-06-04 03:08:52 -0400

    Dashboard elements

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.files.base import File
 from django.db.models.base import Model
 from django.forms.utils import ErrorList
-from .models import Account
+from .models import Account,UserProfile
 
 
 class RegistrationsForms(forms.ModelForm):
@@ -39,3 +39,27 @@ class RegistrationsForms(forms.ModelForm):
            raise forms.ValidationError(
                 "El password no coincide!"
            )
+       
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, error_messages = {'invalid': ('Solo archivos de imagen')}, widget=forms.FileInput)
+    class Meta:
+        model = UserProfile
+        fields = ('address_line_1', 'address_line_2', 'city', 'state', 'country', 'profile_picture')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib import messages,auth
-from .forms import RegistrationsForms
+from .forms import RegistrationsForms,UserProfileForm,UserForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -8,10 +8,12 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
-from .models import Account
+from .models import Account, UserProfile
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
+from orders.models import Order
 import requests 
+
 # Create your views here.
 
 def register (request):
@@ -35,6 +37,11 @@ def register (request):
             user.phone_number =phone_number
             user.save()
             
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
+
             #Mensaje de activacion por email
             current_site= get_current_site(request)
             mail_subjet = "Favor activa tu cuenta en RAANGA"
@@ -50,7 +57,7 @@ def register (request):
             
             messages.success(request,'Se registro el usuario exitosamente')
             
-            return redirect('accounts/login/?command=verification&email='+email)
+            return redirect('/account/login/?command=verification&email='+email)
             # return redirect('register')
 
     context={
@@ -131,7 +138,6 @@ def logout(request):
 
     return redirect('login')
 
-
 def activate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
@@ -150,8 +156,17 @@ def activate(request, uidb64, token):
     
 @login_required(login_url='login')
 def dashboard(request):
-    return render (request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
 
+    # userprofile = UserProfile.objects.get(user_id=request.user.id)
+
+    context = {
+        'orders_count': orders_count,
+        # 'userprofile': userprofile,
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def forgotPassword (request):
     if request.method == "POST":
@@ -195,7 +210,6 @@ def resetPassword_validate(request, uidb64, token):
         messages.error(request, 'El link ha expirado')
         return redirect('login')
 
-    
 def resetPassword(request):
     if request.method == 'POST':
         password = request.POST['password']
@@ -215,3 +229,35 @@ def resetPassword(request):
             return redirect ('resetPassword')
     else:
         return render (request,'accounts/resetPassword.html')
+
+def my_orders(request):
+
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'accounts/my_orders.html', context)
+
+
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Su información fue guardada con éxito')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile': userprofile,
+        'jgj':'jg'
+    }
+
+    return render(request, 'accounts/edit_profile.html', context)
