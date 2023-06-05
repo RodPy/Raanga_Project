d6cc8be424779013c7469a4c5748000a706f3696
RodPy <rodneyrrspy@gmail.com>
2023-06-05 11:27:13 -0400

    postgres Migrate

--- raanga/settings.py
+++ raanga/settings.py
@@ -29,6 +29,7 @@ DEBUG = config('DEBUG',cast=bool)
 ALLOWED_HOSTS = [
     '54.201.234.181',
     '0.0.0.0',
+    '127.0.0.1'
 ]
 
 
@@ -95,13 +96,22 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'raanga',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
